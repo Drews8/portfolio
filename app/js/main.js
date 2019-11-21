00ee911a211f7551100958be57c3511151801fae
Drews8 <prokhorenko.drew@gmail.com>
2019-11-21 23:45:15 +0200

    add grace and gradient animations

--- app/js/main.js
+++ app/js/main.js
@@ -2,50 +2,119 @@
 
 
 	onePageScroll(".main", {
-		sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-		easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", 
-		// "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-		animationTime: 1500,             // AnimationTime let you define how long each section takes to animate
-		pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-		updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-		beforeMove: beforeMoveSlider(),  // This option accepts a callback function. The function will be called before the page moves.
-		afterMove: afterMoveSlider(),   // This option accepts a callback function. The function will be called after the page moves.
-		loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-		keyboard: true,                  // You can activate the keyboard controls
-		responsiveFallback: false        // You can fallback to normal page scroll by defining the width of the browser in which
-		// you want the responsive fallback to be triggered. For example, set this to 600 and whenever 
-		// the browser's width is less than 600, the fallback will kick in.
+		sectionContainer: "section",
+		easing: "ease",
+		animationTime: 1500,
+		pagination: true,
+		updateURL: false,
+		beforeMove: beforeMoveSlider(),
+		afterMove: afterMoveSlider(),
+		loop: false,
+		keyboard: true,
+		responsiveFallback: false
+
 	});
 
-	document.querySelector(".main-aside__scrollbar").style.height = `25%`;
-	document.querySelector(".header").style.animation = `graceIn 1s ease-in-out .5s forwards`;
+	var wow = new WOW(
+		{
+			boxClass: 'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset: 0,          // distance to the element when triggering the animation (default is 0)
+			mobile: true,       // trigger animations on mobile devices (default is true)
+			live: true,       // act on asynchronously loaded content (default is true)
+			callback: function (box) {
+				// the callback is fired every time an animation is started
+				// the argument that is passed in is the DOM node being animated
+			},
+			scrollContainer: null,    // optional scroll container selector, otherwise use window,
+			resetAnimation: true,     // reset animation on end (default is true)
+		}
+	);
+	wow.init();
+
+	let navItems = document.querySelectorAll(".main-nav__item");
+	let animProperties = {
+		fadeInLeft: "fadeOutLeft",
+		fadeOutLeft: "fadeInLeft",
+		fadeInRight: "fadeOutRight",
+		fadeOutRight: "fadeInRight",
+		fadeIn: "fadeOut",
+		fadeOut: "fadeIn",
+
+	}
+
+	document.querySelector(".main-aside__scrollbar").style.height = `20%`;
+	document.querySelectorAll(".main-item")[0].style.animation = `graceIn 1s ease-in-out .5s forwards`;
 	document.querySelectorAll(".main-item__bg")[0].style.animation = `gradientBgIn 1.5s ease-in-out forwards`;
 
+	listenerInit();
+
+
+	function listenerInit() {
+		for (let i = 0; i < navItems.length; i++) {
+			navItems[i].addEventListener("click", scrollTo);
+		}
+	}
 
 	function beforeMoveSlider() {
 		return (index) => {
-			console.log(index);
+
 			let scrollbarHeight = document.querySelector(".main-aside__scrollbar");
 			let preventSlideIndex = parseInt(scrollbarHeight.style.height) / 100 * document.querySelectorAll(".main-item").length - 1;
-			
-			
-			
 
 			scrollbarHeight.style.height = `${index / document.querySelectorAll(".main-item").length * 100}%`;
 			document.querySelectorAll(".main-item__bg")[preventSlideIndex].style.animation = `gradientBgOut 1s ease-in-out forwards`;
 			document.querySelectorAll(".main-item")[preventSlideIndex].style.animation = `graceOut .5s ease-in-out forwards`;
 			document.querySelectorAll(".main-nav__item")[preventSlideIndex].classList.toggle("active");
+//////////////////////////////////////teeeest
+			let contentOut = document.querySelectorAll(".wow");
+			for (let i = 0; i < contentOut.length; i++) {
+				console.log(contentOut[i].classList[2]);
+				let animElement = contentOut[i].classList[2];
 
-			
+				if (document.querySelectorAll(".main-item")[index - 1].classList[1] === contentOut[i].classList[0].replace(/_.*/, '')) {
+					contentOut[i].classList.remove(animElement);
+					contentOut[i].classList.add(animProperties[animElement]);
+					contentOut[i].style.animation = `${animProperties[animElement]} ${0.1}s forwards`;
+
+				} else {
+
+					contentOut[i].classList.remove(animElement);
+					contentOut[i].classList.add(animProperties[animElement]);
+					contentOut[i].style.animation = `${animProperties[animElement]} 1s forwards`;
+					//${contentOut[i].getAttribute("data-wow-duration")}
+				}
+			}
+
 		}
 	}
+
 	function afterMoveSlider() {
 		return (index) => {
 			document.querySelectorAll(".main-item__bg")[index - 1].style.animation = `gradientBgIn 1s ease-in-out forwards`;
 			document.querySelectorAll(".main-item")[index - 1].style.animation = `graceIn .5s ease-in-out forwards`;
-			document.querySelectorAll(".main-nav__item")[index-1].classList.toggle("active");
+			document.querySelectorAll(".main-nav__item")[index - 1].classList.toggle("active");
+
+			let contentOut = document.querySelectorAll(".wow");
+			for (let i = 0; i < contentOut.length; i++) {
+				let animElement = contentOut[i].classList[2];
+				//if (document.querySelectorAll(".main-item")[index - 1].classList[1] === contentOut[i].classList[0].replace(/_.*/, '')) {}
+
+
+				contentOut[i].classList.remove(animElement);
+				contentOut[i].classList.add(animProperties[animElement]);
+				contentOut[i].style.visibility = `visible`;
+				// console.log(contentOut[i].getAttribute("data-wow-duration"));
+				contentOut[i].style.animation = `${animProperties[animElement]} ${contentOut[i].getAttribute("data-wow-duration")} forwards`;
+				
+			}
 		}
 	}
 
+	function scrollTo() {
+
+		let index = Array.from(this.parentNode.children).indexOf(this);
+		moveTo("main", index + 1);
+	}
 
 })();
